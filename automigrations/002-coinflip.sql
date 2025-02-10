f2b6c4798728d4d5339bdbc7b19c949139b2dd05
Dan Zhou <danzho@proton.me>
2025-02-10 15:07:19 -0600

    Bump caas and add fk indexes

--- automigrations/002-coinflip.sql
+++ automigrations/002-coinflip.sql
@@ -1,5 +1,6 @@
 -- Example bet table
 create table app.coinflip_bet (
+  -- UUIDv7 gives us time-ordered random UUIDs
   id           uuid  primary key default caas_hidden.uuid_generate_v7(),
   wager        float not null,
   heads        boolean not null,
@@ -7,7 +8,7 @@ create table app.coinflip_bet (
   net          float not null, -- negative if lost, wager*(multiplier-1) if won
   currency_key text  not null, -- e.g. "BTC", "HOUSE"
 
-  -- Remember: the caas database is multi-tenant across every experience on every Moneypot casino.
+  -- Let us easily look up bets per casino and per experience
   user_id       uuid not null references caas.user(id),
   casino_id     uuid not null references caas.casino(id),
   experience_id uuid not null references caas.experience(id),
@@ -15,3 +16,8 @@ create table app.coinflip_bet (
   -- Currencies are unique per casino
   foreign key (currency_key, casino_id) references caas.currency(key, casino_id)
 );
+
+-- Note: Adding foreign key indexes also generates relational queries in our graphql API
+create index coinflip_bet_user_id_idx on app.coinflip_bet(user_id);
+create index coinflip_bet_casino_id_idx on app.coinflip_bet(casino_id);
+create index coinflip_bet_experience_id_idx on app.coinflip_bet(experience_id);
